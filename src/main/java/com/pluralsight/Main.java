c80b11da93aa75b39f24eefddd299e06a7d28bf8
Yosh Ntigahera <Yntigahera@gmail.com>
2024-10-16 11:14:48 -0700

    update my payment method to ask only 2 questions and also display negative amount. Also added a sort by date meethod i'm trying to work on.

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -18,70 +18,71 @@ public class Main {
 //    String time;
 //    String description;
     String vendor;
-//    String amount;
+    //    String amount;
     static Scanner myscanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
 
         displayMenu();
+
+
     }
 
 
-     public static void displayMenu() throws IOException {
+    public static <Transaction> void displayMenu() throws IOException {
 
-         String options;
-         System.out.println("Good afternoon what would you like to do?"); //Later will add if time is am print more if it is pm print good afternoon
-         System.out.println("D) Add Deposit \nP) Make payment (Debit) \nL) Ledger \nX) Exit");
-         options = myscanner.nextLine();
+        String options;
+        System.out.println("Good afternoon what would you like to do?"); //Later will add if time is am print more if it is pm print good afternoon
+        System.out.println("D) Add Deposit \nP) Make payment (Debit) \nL) Ledger \nX) Exit");
+        options = myscanner.nextLine();
 
-         if (options.equalsIgnoreCase("D")) {
-             addDeposit();
-         } else if (options.equalsIgnoreCase("P")) {
-             makePayment();
-         } else if (options.equalsIgnoreCase("L")) {
-             Ledger();
-         } else if (options.equalsIgnoreCase("X")) {
-             System.out.println("Exiting... Have a great day");
-         }
-     }
+        if (options.equalsIgnoreCase("D")) {
+            addDeposit();
+        } else if (options.equalsIgnoreCase("P")) {
+            makePayment();
+        } else if (options.equalsIgnoreCase("L")) {
+            Ledger();
+        } else if (options.equalsIgnoreCase("X")) {
+            System.out.println("Exiting... Have a great day");
+        }
+    }
 
 
 
-        public static void writeToCSV() {
-            File file = new File("transactions.csv");
 
-            try {
-                if (file.createNewFile()) {
-                    System.out.println("file created: " + file.getName());
-                }
-                // Reading from the file
-                try (Scanner scanner = new Scanner(file)) {
-                    while (scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        System.out.println(line); // Display the content
-                    }
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred");
-                e.printStackTrace();
+    public static void writeToCSV() {
+        File file = new File("transactions.csv");
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("file created: " + file.getName());
             }
+            // Reading from the file
+            try (Scanner scanner = new Scanner(file)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(line); // Display the content
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred");
+            e.printStackTrace();
+        }
     }
 
 
-    public static void makePayment () throws IOException {
+    public static void makePayment() throws IOException {
         System.out.println("How much of your balance would you like to pay now: ");
         double payment = myscanner.nextDouble();
         myscanner.nextLine();
-        System.out.println("Enter card Number: ");
-        double cardInfo = myscanner.nextDouble();
-        System.out.println("Enter expiration date: ");
-       myscanner.nextLine();
-        String expirationDate = myscanner.nextLine();
-        System.out.println("Enter CVV: ");
-        int cvv = myscanner.nextInt();
-        myscanner.nextLine();
+        System.out.println("Enter Name of company: ");
+        String name = myscanner.nextLine();
         //-------------------------------------------------------------------------------------------
-        String cardinformation = cardInfo + "|" + payment + "|" + expirationDate;
+        LocalDate todayDate = LocalDate.now();
+        LocalTime todayTime = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
+        //-------------------------------------------------------------------------------------------
+
+        String cardinformation = todayDate + "|" + todayTime + "|" + name + "|" + (-payment); //display input that was entered with negative
         BufferedWriter cw = new BufferedWriter(new FileWriter("transactions.csv", true));
         cw.newLine();
         cw.write(cardinformation);
@@ -89,8 +90,8 @@ public class Main {
         cw.close();
     }
 
-    public static void addDeposit () throws IOException {
-        System.out.println("Enter amount you would like to deposit:");
+    public static void addDeposit() throws IOException {
+        System.out.println("Enter amount you would like to deposit: ");
         double deposit = myscanner.nextDouble();
         myscanner.nextLine();
         System.out.println("Description: ");
@@ -104,56 +105,91 @@ public class Main {
         //-----------------------------------------------------------------------------------------------
         String transcation = todayDate + "|" + todayTime + "|" + description + "|" + name + "|" + deposit;
         //-----------------------------------------------------------------------------------------------
-        BufferedWriter cw = new BufferedWriter(new FileWriter ("transactions.csv", true)); //Going over file to apend string into the file
+        BufferedWriter cw = new BufferedWriter(new FileWriter("transactions.csv", true)); //Going over file to apend string into the file
         cw.write(transcation);
         //Close it because the program is going to think i am still writing stuff basically waiting for it to be done. in while loop until closed.
         cw.close();
 
     }
 
-    public static void Ledger () throws IOException {
+    public static void Ledger() throws IOException {
         String ledgerOptions;
         System.out.println("A) All \nD) Deposit \nP) Payments \nR) Reports");
         ledgerOptions = myscanner.nextLine();
 
         if (ledgerOptions.equalsIgnoreCase("A")) {
-            displayAll();
+
+
         } else if (ledgerOptions.equalsIgnoreCase("D")) {
             allDeposits();
+//            sortTransactionsByDate();
         } else if (ledgerOptions.equalsIgnoreCase("P")) {
-            System.out.println();
+            makePayment();
         } else if (ledgerOptions.equalsIgnoreCase("R")) {
             System.out.println();
         } else {
             System.out.println("Invalid option");
         }
     }
-
-    private static void displayAll (){
+//For loop when i get back
+    private static void allPayments() {
         File file = new File("transactions.csv");
-
-            try (Scanner scanner = new Scanner(file)) {
-                System.out.println("All Transactions: ");
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine();
-                    System.out.println(line);
-                }
-            } catch (IOException e) {
-                System.out.println("An error occurred while reading transactions.");
-                e.printStackTrace();
-            }
-
-        
-    }
-
-    private static void allDeposits() {
-        File file = new File("transactions.csv");
-
+        boolean isFirstLine = true;
         try (Scanner scanner = new Scanner(file)) {
             System.out.println("Deposits:");
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                String[] fields = line.split("\\|"); // Split by the delimiter
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] fields = line.split("\\|");
+                // Split by the delimiter
+                if (fields.length > 2) { // Check if there are enough fields
+                    String amountInString = fields[fields.length - 1]; // Last field is the amount
+                    BufferedWriter cw = new BufferedWriter(new FileWriter("transactions.csv", true));
+                    double amount = Double.parseDouble(amountInString);
+                    if (amount > 0) { // Check if the amount is a deposit
+                        System.out.println(line);
+                        cw.write(line + System.lineSeparator());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading transactions.");
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void displayAll() {
+        File file = new File("transactions.csv");
+
+        try (Scanner scanner = new Scanner(file)) {
+            System.out.println("All Transactions: ");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading transactions.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void allDeposits() {
+        File file = new File("transactions.csv");
+        boolean isFirstLine = true;
+        try (Scanner scanner = new Scanner(file)) {
+            System.out.println("Deposits:");
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (isFirstLine) {
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] fields = line.split("\\|");
+                // Split by the delimiter
                 if (fields.length > 4) { // Check if there are enough fields
                     String amountInString = fields[fields.length - 1]; // Last field is the amount
                     double amount = Double.parseDouble(amountInString);
@@ -166,6 +202,12 @@ public class Main {
             System.out.println("An error occurred while reading transactions.");
             e.printStackTrace();
         }
+
+
     }
 
+    public static void sortTransactionsByDate(List<Search> transactions) {
+        transactions.sort((t1, t2) -> t2.getStartDate().compareTo(t1.getStartDate()));
+
+    }
 }
--- src/main/java/com/pluralsight/Search.java
+++ src/main/java/com/pluralsight/Search.java
@@ -4,13 +4,15 @@ public class Search {
     String startDate;
     String endDate;
     String description;
+    String name;
     double amount;
 
-    public Search(String _startDate, String _endDate, String _description, double _amount){
+    public Search(String _startDate, String _endDate, String _description, double _amount, String _name){
         this.startDate = _startDate;
         this.endDate = _endDate;
         this.description = _description;
         this.amount = _amount;
+        this.name = _name;
     }
 
     public Search(){
@@ -45,5 +47,8 @@ public class Search {
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
+    public double getName(){return amount; }
+    public void setName(double amount){this.amount = amount; }
 
 }
